52be1a9fe87c7ba8dfe3bbfe7e33c257a4ca8f36
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-08 12:31:13 -0600

    tweaks favorite food statement

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -2,5 +2,5 @@
 //
 // console.log("I love to eat " + "various meat pizzas " + "with pineapple! " + "The pineapple adds a wonderful sweetness that is otherwise, by itself, " + "just salty.");
 
-var statement  = "My favorite food is ", favoriteFood = "pizza with pineapple"
+var statement  = "\tMy favorite food is...\n", favoriteFood = "pizza \n\twith \n\t\tpineapple"
 console.log(statement + favoriteFood + "!");
