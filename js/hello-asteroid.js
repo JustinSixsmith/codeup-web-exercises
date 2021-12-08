5ef8797c7109003f87c5ea7cf60498d2a32ceb43
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-08 12:01:03 -0600

    adds hello-astroid

---
+++ js/hello-asteroid.js
@@ -0,0 +1,6 @@
+// console.log("hello, sweet asteroid!");
+//
+// console.log("I love to eat " + "various meat pizzas " + "with pineapple! " + "The pineapple adds a wonderful sweetness that is otherwise, by itself, " + "just salty.");
+
+var statement  = "My favorite food is ", favoriteFood = "pizza with pineapple"
+console.log(statement + favoriteFood + "!");
