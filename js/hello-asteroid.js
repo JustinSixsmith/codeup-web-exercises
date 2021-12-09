43a06bd648ae4fc442e7f52ccaf35da1bb871975
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-09 12:21:14 -0600

    adds beginner exercises

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -2,5 +2,7 @@
 //
 // console.log("I love to eat " + "various meat pizzas " + "with pineapple! " + "The pineapple adds a wonderful sweetness that is otherwise, by itself, " + "just salty.");
 
-var statement  = "\tMy favorite food is...\n", favoriteFood = "pizza \n\twith \n\t\tpineapple"
+let statement  = "\tMy favorite food is...\n", favoriteFood = "pizza \n\twith \n\t\tpineapple"
 console.log(statement + favoriteFood + "!");
+
+
---
+++ js/types-operators-variables.js
@@ -0,0 +1,60 @@
+// var a = 1;
+// var b = a++;
+// var c = ++a;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var d = "hello";
+// var e = false;
+// d++;
+// e++;
+// console.log(d);
+// console.log(e);
+
+// var perplexed;
+// console.log(perplexed + 2);
+
+// var price = 2.7;
+// console.log(price.toFixed(2));
+
+// console.log(isNaN(0));
+// console.log(isNaN(1));
+// console.log(isNaN(""));
+// console.log(isNaN("string"));
+// console.log(isNaN("0"));
+// console.log(isNaN("1"));
+// console.log(isNaN("3.145"));
+// console.log(isNaN(Number.MAX_VALUE));
+// console.log(isNaN(Infinity));
+// console.log(isNaN("true"));
+// console.log(isNaN(true));
+// console.log(isNaN("false"));
+// console.log(isNaN(false));
+// NaN === NaN;
+
+// console.log(!true);
+// console.log(!false);
+// console.log(!!true);
+// console.log(!!false);
+// console.log(!!0);
+// console.log(!!-0);
+// console.log(!!1);
+// console.log(!!-1);
+// console.log(!!0.1);
+// console.log(!!"hello");
+// console.log(!!"");
+// console.log(!!'');
+// console.log(!!"false");
+// console.log(!!"0");
+
+var sample = "Hello Codeup";
+console.log(sample.length);
+console.log(sample.toUpperCase());
+console.log(sample.toLowerCase());
+console.log(sample);
+console.log(sample += " Students");
+console.log(sample.replace("Students", "CLass"));
+console.log(sample.indexOf("c"));
+console.log(sample.indexOf("C"));
+console.log(sample.substr(6, 6));
