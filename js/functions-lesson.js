f5ec38c2f8448ee8fadca2626f8a72b527238f77
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-09 21:22:19 -0600

    adds functions lesson worksheet

---
+++ js/functions-lesson.js
@@ -0,0 +1,67 @@
+// function increment(x) {
+//     return x + 1;
+// }
+//
+// let four = increment(3);
+// let six = increment(increment(increment(3)));
+//
+// console.log(four);
+// console.log(six);
+
+
+// let increment = function(x) {
+//     return x + 1;
+// };
+//
+// let two = increment(1);
+//
+// console.log(two);
+
+
+// function sum(a, b) {
+//     var result = a + b;
+//     return result;
+// }
+//
+// let x = 3;
+// let seven = sum(x, 4);
+//
+// console.log(seven);
+
+
+// let globalLet = "Look, I'm Global!";
+//
+// function sayHello() {
+//     alert(globalLet);
+// }
+//
+// sayHello();
+
+
+// let globalLet = "Look, I'm Global!";
+//
+// function scopeExample() {
+//     let localLet = "Look, I'm Local!";
+//     alert(localLet);
+//     alert(globalLet);
+// }
+//
+// scopeExample();
+// alert(globalLet);
+
+
+// let x = 300;
+// let y = 100;
+//
+// function scopeExample() {
+//     let x = 1;
+//     let y = 2;
+//     console.log('x: ' + x + ' y: ' + y);
+//     return x + y;
+// }
+//
+// console.log('x: ' + x + ' y: ' + y);
+// let returnValue = scopeExample();
+// console.log(returnValue);
+
+
