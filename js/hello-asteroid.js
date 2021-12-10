93288713dbdaaea2e318da879371e7fde409829a
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-09 18:10:43 -0600

    finishes introduction exercises

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,8 +1,7 @@
 // console.log("hello, sweet asteroid!");
 //
 // console.log("I love to eat " + "various meat pizzas " + "with pineapple! " + "The pineapple adds a wonderful sweetness that is otherwise, by itself, " + "just salty.");
-
-let statement  = "\tMy favorite food is...\n", favoriteFood = "pizza \n\twith \n\t\tpineapple"
-console.log(statement + favoriteFood + "!");
-
+//
+// let statement  = "\tMy favorite food is...\n", favoriteFood = "pizza \n\twith \n\t\tpineapple"
+// console.log(statement + favoriteFood + "!");
 
--- js/types-operators-variables.js
+++ js/types-operators-variables.js
@@ -47,14 +47,42 @@
 // console.log(!!'');
 // console.log(!!"false");
 // console.log(!!"0");
+//
+// var sample = "Hello Codeup";
+// console.log(sample.length);
+// console.log(sample.toUpperCase());
+// console.log(sample.toLowerCase());
+// console.log(sample);
+// console.log(sample += " Students");
+// console.log(sample.replace("Students", "CLass"));
+// console.log(sample.indexOf("c"));
+// console.log(sample.indexOf("C"));
+// console.log(sample.substring(sample.indexOf("C"), sample.indexOf("p") + 1));
 
-var sample = "Hello Codeup";
-console.log(sample.length);
-console.log(sample.toUpperCase());
-console.log(sample.toLowerCase());
-console.log(sample);
-console.log(sample += " Students");
-console.log(sample.replace("Students", "CLass"));
-console.log(sample.indexOf("c"));
-console.log(sample.indexOf("C"));
-console.log(sample.substr(6, 6));
+// let userAge = "32";
+
+// console.log(parseInt(inStr));
+
+// let perDiem = 3, littleMermaid = 3, brotherBear = 5, hercules = 1;
+// console.log(amountOwed = ("You owe ") + ("$") + (perDiem * (littleMermaid + brotherBear + hercules)));
+//
+// let googleRate = 400, amazonRate = 380, facebookRate = 350;
+// let googleHrs = 6, amazonHrs = 4, facebookHrs = 10;
+// console.log(amountPaid = ("You made ") + ("$") + (googleRate * googleHrs + amazonRate * amazonHrs + facebookRate * facebookHrs));
+
+// let notFull = false, scheduleClear = true;
+// console.log(canEnroll = (notFull) && (scheduleClear));
+
+// let notExpired = true;
+// let minimumItems = 3;
+// let premiumMember = true;
+//
+// console.log((notExpired) && (minimumItems >= 2) || (premiumMember));
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+// console.log(password.length >= 5);
+// console.log(!password.includes(username));
+// console.log(username.length <= 20);
+console.log(!username.startsWith(" ") && !username.endsWith(" ") && !password.startsWith(" ") && !password.endsWith(" "));
