59833110a1f96308162960064c8570fe3e6e6d35
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-28 19:59:04 -0600

    loops exercise #2

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,6 @@
+
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
 function showMultiplicationTable(numberToMultiply) {
     for (let i = 1; i <= 10; i++) {
         console.log(numberToMultiply + " x " + i + " = " + (numberToMultiply * i));
@@ -5,3 +8,7 @@ function showMultiplicationTable(numberToMultiply) {
 }
 
 showMultiplicationTable(2);
+
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+
