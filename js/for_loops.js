8e2fd96bea23b2b919d70e3437f1ad37271b55c5
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-28 19:54:58 -0600

    adds loops exercise #1

---
+++ js/for_loops.js
@@ -0,0 +1,7 @@
+function showMultiplicationTable(numberToMultiply) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(numberToMultiply + " x " + i + " = " + (numberToMultiply * i));
+    }
+}
+
+showMultiplicationTable(2);
