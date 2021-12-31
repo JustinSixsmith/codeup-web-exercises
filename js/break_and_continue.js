f80ae497d3e0ec21a29e11190d9e0548d02f06cb
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-30 23:49:30 -0600

    added breaks and continue exercise #3

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,29 @@
 // Exercise #2: Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 
-let maybeOdd = prompt("Enter an odd number between 1 and 50.");
+// let maybeOdd = prompt("Enter an odd number between 1 and 50.");
+//
+// while (isNaN(maybeOdd) || maybeOdd < 1 || maybeOdd > 50 || maybeOdd % 2 === 0) {
+//     alert("Invalid entry. Please try again.");
+//     maybeOdd = prompt("Enter an odd number between 1 and 50.");
+// }
+//
+// alert("Thank you. " + maybeOdd + " is an odd number between 1 and 50.");
 
-while (isNaN(maybeOdd) || maybeOdd < 1 || maybeOdd > 50 || maybeOdd % 2 === 0) {
-    alert("Invalid entry. Please try again.");
-    maybeOdd = prompt("Enter an odd number between 1 and 50.");
+
+
+// Exercise #3: Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+console.log("Number to skip is: 27");
+console.log("")
+
+for (let i = 1; i < 100; i++) {
+
+    if (i === 27) {
+        console.log("Yikes! Skipping number: 27");
+        continue;
+    } else if (i % 2 === 0) {
+        continue;
+    }
+
+    console.log('Here is an odd number: ' + i);
 }
-
-alert("Thank you. " + maybeOdd + " is an odd number between 1 and 50.");
-
-
-
-// Exercise #3:
-
