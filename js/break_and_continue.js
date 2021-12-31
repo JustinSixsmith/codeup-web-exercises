70cff70e8bee38b3c3154243602c108aae6468e4
Justin Sixsmith <justinsixsmith@gmail.com>
2021-12-30 23:35:50 -0600

    fixed the exercise to provide alerts

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,15 @@
 // Exercise #2: Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 
-do {
+let maybeOdd = prompt("Enter an odd number between 1 and 50.");
+
+while (isNaN(maybeOdd) || maybeOdd < 1 || maybeOdd > 50 || maybeOdd % 2 === 0) {
+    alert("Invalid entry. Please try again.");
     maybeOdd = prompt("Enter an odd number between 1 and 50.");
-} while ((maybeOdd > 50) || (maybeOdd % 2 === 0));
+}
+
+alert("Thank you. " + maybeOdd + " is an odd number between 1 and 50.");
+
+
+
+// Exercise #3:
+
