1b4f18dc1094413fadceb11d01bb55f1aaf03a38
Justin Sixsmith <justinsixsmith@gmail.com>
2022-01-05 12:28:38 -0600

    adds planets string exercises

--- js/planets-array.js
+++ js/planets-array.js
@@ -27,16 +27,22 @@
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    let earthIndex = planets.indexOf("Earth");
+    console.log(earthIndex);
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
