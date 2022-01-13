f53f33931eaadf56a363266d06c7fbf402015492
Justin Sixsmith <justinsixsmith@gmail.com>
2022-01-13 09:19:05 -0600

    adds arrayToString warmup exercise

--- js/warmups.js
+++ js/warmups.js
@@ -45,3 +45,21 @@
 // console.log(isMultipleOfThree(48));
 
 
+// Create a function called arrayToString.
+//     The function takes a parameter called myArray.
+//     It returns a string that is the concatenation of all the elements in myArray.
+//     This function is essentially your own version of join.
+//     Therefore, you cannot use join in your function body :slightly_smiling_face:
+
+
+
+
+function arrayToString(myArray) {
+    let arrayString = "";
+    for (let i = 0; i < myArray.length; i++) {
+        arrayString += myArray[i];
+    }
+    return arrayString;
+}
+
+console.log(arrayToString(["asl", "woj", 3, 4]));
