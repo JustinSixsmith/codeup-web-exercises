32b2c1dcc22f24100d2977002c05a348ca044934
Justin Sixsmith <justinsixsmith@gmail.com>
2022-02-18 10:54:20 -0600

    adds promise exercises

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,40 @@
-const gitData = fetch('https://api.github.com/users', {headers: {'Authorization': `token ${MY_GITHUB_API_KEY}`}})
-    .then(response => response.json())
-    .catch(error => console.error(error));
+// function getAllUsers() {
+//     fetch('https://api.github.com/users', {headers: {'Authorization': `token ${MY_GITHUB_API_KEY}`}})
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error));
+// }
+//
+// getAllUsers();
 
-console.log(gitData);
+
+function getEventDateByUserName(userName) {
+    return fetch('https://api.github.com/users/' + userName + '/events/public', {headers: {'Authorization': `token ${MY_GITHUB_API_KEY}`}})
+        .then(response => response.json())
+        .then(data => new Date(data[0].created_at).toDateString())
+        .catch(error => console.error(error));
+}
+
+getEventDateByUserName("JustinSixsmith").then(event => console.log(event));
+
+
+
+function wait(aNum) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.2) {
+                let time = aNum / 1000;
+                resolve("You'll see this after " + time + " seconds.");
+            } else {
+                reject('Your request was denied!');
+            }
+        }, aNum);
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+const request = wait(5000);
+request.then(message => console.log(message));
+request.catch(message => console.log(message));
