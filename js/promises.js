71e2c1dfe32d9b00b99345b4eb8fe14df09d3b03
Justin Sixsmith <justinsixsmith@gmail.com>
2022-02-18 12:17:50 -0600

    refactors promise exercise to filter out push events

--- js/promises.js
+++ js/promises.js
@@ -1,40 +1,48 @@
-// function getAllUsers() {
-//     fetch('https://api.github.com/users', {headers: {'Authorization': `token ${MY_GITHUB_API_KEY}`}})
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.error(error));
-// }
-//
-// getAllUsers();
+"use strict";
 
-
-function getEventDateByUserName(userName) {
+function getUserLastPushEvent(userName) {
     return fetch('https://api.github.com/users/' + userName + '/events/public', {headers: {'Authorization': `token ${MY_GITHUB_API_KEY}`}})
         .then(response => response.json())
-        .then(data => new Date(data[0].created_at).toDateString())
-        .catch(error => console.error(error));
+        .then(events => {
+            for (let event of events) {
+                if (event.type === "PushEvent") {
+                    return new Date(event.created_at).toDateString();
+                }
+            }
+        }).catch(error => console.error(error));
 }
 
-getEventDateByUserName("JustinSixsmith").then(event => console.log(event));
+getUserLastPushEvent("JustinSixsmith").then(dateOfEvent => console.log(dateOfEvent));
 
 
 
-function wait(milliseconds) {
+// function wait(milliseconds) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (Math.random() > 0.1) {
+//                 let seconds = milliseconds / 1000;
+//                 resolve("You'll see this after " + seconds + " seconds.");
+//             } else {
+//                 reject('Your request was denied!');
+//             }
+//         }, milliseconds);
+//     });
+// }
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//
+// const request = wait(5000);
+// request.then(message => console.log(message));
+// request.catch(message => console.log(message));
+
+
+const wait = milliseconds => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (Math.random() > 0.1) {
-                let seconds = milliseconds / 1000;
-                resolve("You'll see this after " + seconds + " seconds.");
-            } else {
-                reject('Your request was denied!');
-            }
-        }, milliseconds);
-    });
-}
+            resolve(milliseconds);
+        }, milliseconds)
+    })
+};
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
-
-const request = wait(5000);
-request.then(message => console.log(message));
-request.catch(message => console.log(message));
+wait(5000).then((ms) => console.log(`You'll see this after ${ms/1000} seconds.`));
